473647e5d5365c23053919d562c4e178919d8053
pasipasipada <irfanoputra27@gmail.com>
2024-11-08 21:08:51 +0700

    feat: add integration api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -31,6 +32,33 @@ import NotFoundView from "./pages/NotFoundView";
 //   },
 // ]);
 function App() {
+  // store data sedara state react nya
+  const [shops, setShops] = useState([]);
+  const [error, setError] = useState(null);
+
+  console.log(shops);
+
+  // fetch data, fetch / axios
+  useEffect(() => {
+    const fetchShops = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/v1/shops");
+        console.log(response);
+
+        const data = response.data;
+        if (data.isSuccess) {
+          setShops(data.data.shops);
+        } else {
+          setError("error");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchShops();
+  }, []);
+
   return (
     <>
       {/* <RouterProvider router={router} /> */}
@@ -57,32 +85,43 @@ function App() {
           Register
         </button>
       </header>
-
-      <section className="max-w-6xl mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {" "}
-        {[...Array(4)].map((_, index) => (
-          <div key={index} className="p-4 border rounded-md bg-white shadow-md">
+      <main className="text-center">
+        {/* {loading && <P> loading...</P>} */}
+        {error && <p className="text-red-500">Error: {error}</p>}
+        {!error && (
+          <section className="max-w-6xl mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {" "}
-            <img
-              src="/path-to-image/car.png"
-              alt="Car"
-              className="w-full h-40 object-cover mb-4"
-            />{" "}
-            <h3 className="font-semibold">Nama/Tipe Mobil</h3>{" "}
-            <p className="text-green-500 font-bold">Rp 430.000 / hari</p>{" "}
-            <p className="text-gray-600 mt-2 text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>{" "}
-            <div className="flex items-center justify-between text-gray-500 text-sm mt-4">
-              {" "}
-              <span>4 orang</span> <span>Manual</span> <span>Tahun 2020</span>{" "}
-            </div>{" "}
-            <button className="w-full px-4 py-2 mt-4 text-white bg-green-500 rounded-md">
-              Pilih Mobil
-            </button>{" "}
-          </div>
-        ))}{" "}
-      </section>
+            {shops.map((shop, index) => (
+              <div
+                key={index}
+                className="p-4 border rounded-md bg-white shadow-md"
+              >
+                {" "}
+                <img
+                  src={shop.products[0].images[0]}
+                  alt={shop.products[0].name}
+                  className="w-full h-40 object-cover mb-4"
+                />{" "}
+                <h3 className="font-semibold">{shop.products[0].name}</h3>{" "}
+                <p className="text-green-500 font-bold">
+                  Rp {shop.products[0].price} / hari
+                </p>{" "}
+                <p className="text-gray-600 mt-2 text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>{" "}
+                <div className="flex items-center justify-between text-gray-500 text-sm mt-4">
+                  {" "}
+                  <span>4 orang</span> <span>Manual</span>{" "}
+                  <span>Tahun 2020</span>{" "}
+                </div>{" "}
+                <button className="w-full px-4 py-2 mt-4 text-white bg-green-500 rounded-md">
+                  Pilih Mobil
+                </button>{" "}
+              </div>
+            ))}{" "}
+          </section>
+        )}
+      </main>
     </>
   );
 }
